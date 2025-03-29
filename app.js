1e491327998349fd83a1c7c772cbc586abfdf57d
EdoardoProstamo <edoardoprostamo.19@libero.it>
2025-03-29 15:37:21 +0100

    feat: impostazione routers + readme

--- app.js
+++ app.js
@@ -2,6 +2,15 @@ const expressFile = require('express');
 const app = expressFile();
 const port = 2800;
 
+//richiamo di routers
+const dessertRouter = require('./routers/rotte.js');
+
+//rotta '/rotte.js' richiamata da routers
+app.use('/rotte.js', dessertRouter);
+// asset statici
+app.use(expressFile.static('public'));
+
+
 // server
 app.listen(port, () => {
     console.log('Attivazione del server sulla porta ' + port);
--- routers/rotte.js
+++ routers/rotte.js
@@ -1,17 +1,21 @@
+const posts = require('./data/lista-dessert.js');
+const router = expressFile.Router();
 const expressFile = require('express');
-const router = expressFile.Router(); // eseguo 'Router' che esiste dentro express
-const post = require('./data/script.js');
 
-
-// rotta '/'
-app.get('/', (req, res) => {
-    res.send('Rotta principale');
+// rotta '/'  (index)
+router.get('/', (req, res) => {
+    res.send('Lista dei dessert disponibili');
 });
 
-// rotta '/dessert'
+
+
+// rotta '/dessert' + json
 app.get('/dessert', (req, res) => {
-    res.send('Rotta secondaria');
+    res.json(posts);
 });
+
+
+
 
 
 module.exports = router;// esporto router
